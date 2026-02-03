ecc54ae5cc794f7db5e6dcdcdd0eeba8f9f7e820
Varunappalla1993 <an.varun1993@gmail.com>
2026-02-03 16:43:37 +0530

    cat

--- common.sh
+++ common.sh
@@ -4,15 +4,19 @@
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
+
 # colors
 NORMAL='\e[0m'
 RED='\e[31m'
 GREEN='\e[32m'
 BLUE='\e[33m'
 
-START_TIME=$(date +%s)
-mkdir -p $LOGS_FOLDER
+SCRIPT_DIR=$PWD 
 
+START_TIME=$(date +%s)
+MONGODB_HOST=mongodb.vardevops.online
+
+mkdir -p $LOGS_FOLDER
 
 echo "$(date "+%Y-%m-%d %H:%M:%S") || Script started executing at: $(date)" | tee -a $LOGS_FILE
 
@@ -34,6 +38,68 @@ else
     echo -e "$(date "+%Y-%m-%d %H:%M:%S") || $2... $GREEN success $NORMAL" | tee -a $LOGS_FILE
 fi
 }
+
+nodejs_setup()
+{
+    dnf module disable nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "disabling node js default version"
+
+    dnf module enable nodejs:20 -y &>>$LOGS_FILE
+    VALIDATE $? "enabling node js 20 version"
+
+    dnf install nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "installing node js"
+
+    npm install &>>$LOGS_FILE
+    VALIDATE $? "Install dependencies"
+}
+
+app_setup()
+{
+    id roboshop &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+        VALIDATE $? "Creating system user"
+    else
+        echo -e "Roboshop user exists, hence $RED skipping $NORMAL"
+    fi
+
+    mkdir -p /app 
+    VALIDATE $? "Creating directory"
+
+    curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$LOGS_FILE
+    VALIDATE $? "Downloading $app_name code"
+
+    cd /app 
+    VALIDATE $? "Moving to app directory"
+
+    rm -rf /app/*
+    VALIDATE $? "Remove existing code"
+
+    unzip /tmp/$app_name.zip &>>$LOGS_FILE
+    VALIDATE $? "Unzipping the $app_name code"
+}
+
+systemd_setup()
+{
+    cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service
+    VALIDATE $? "Created systemctl catalogue service"
+
+    systemctl daemon-reload
+    VALIDATE $? "daemon reload"
+
+    systemctl enable $app_name &>>$LOGS_FILE
+    systemctl start $app_name &>>$LOGS_FILE
+    VALIDATE $? "enabling and starting $app_name"
+
+}
+
+app_restart()
+{
+    systemctl restart $app_name &>>$LOGS_FILE
+    VALIDATE $? "Restarting $app_name"
+}
+
 
 print_total_time()
 {
