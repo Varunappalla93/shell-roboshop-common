0d838c3c493b5c9d902333f18a4d937d3171fda1
Varunappalla1993 <an.varun1993@gmail.com>
2026-02-03 16:13:09 +0530

    MDB

---
+++ common.sh
@@ -0,0 +1,43 @@
+# Day 19:
+
+#!/bin/bash
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+# colors
+NORMAL='\e[0m'
+RED='\e[31m'
+GREEN='\e[32m'
+BLUE='\e[33m'
+
+START_TIME=$(date +%s)
+
+echo "$(date "+Y%-%m-%d %H:%M:%S") || Script started executing at: $(date)" | tee -a $LOGS_FILE
+
+check_root()
+{
+if [ $USERID -ne 0 ]; then
+    echo -e "$RED Pls run this script with root user access $NORMAL"
+    exit 1
+fi
+}
+
+mkdir -p $LOGS_FOLDER
+
+# Validate function
+VALIDATE()
+{
+if [ $1 -ne 0 ]; then
+    echo -e "$(date "+Y%-%m-%d %H:%M:%S") || $2... $RED failed $NORMAL" | tee -a $LOGS_FILE
+    exit 1
+else
+    echo -e "$(date "+Y%-%m-%d %H:%M:%S") || $2... $GREEN success $NORMAL" | tee -a $LOGS_FILE
+fi
+}
+
+print_total_time()
+{
+    END_TIME=$(date +%s)
+    TOTAL_TIME=$(($END_TIME-$START_TIME))
+    echo -e "$(date "+Y%-%m-%d %H:%M:%S") || Script executed at: $TOTAL_TIME seconds" | tee -a $LOGS_FILE
+}
---
+++ mongodb.sh
@@ -0,0 +1,26 @@
+# Day 19:
+#!/bin/bash
+
+source ./common.sh
+
+check_root
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copying mongo repo"
+
+dnf install mongodb-org -y &>>$LOGS_FILE
+VALIDATE $? "Installing mongodb server"
+
+systemctl enable mongod &>>$LOGS_FILE
+VALIDATE $? "Enable mongodb"
+
+systemctl start mongod &>>$LOGS_FILE
+VALIDATE $? "Start mongodb"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections"
+
+systemctl restart mongod
+VALIDATE $? "Restarted mongodb"
+
+print_total_time
